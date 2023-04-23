ae065dae29b97b608fb46b3d9d228ae51d2cee77
Manon Fernandez <fernandez.manon23230@gmail.com>
2023-04-23 13:09:31 +0200

    declaration of initial variables

---
+++ JS/functions.js
@@ -0,0 +1,19 @@
+/**Variables initiales */
+let playerActive;
+let inGame = false;
+let currentScore = 0;
+let globalScore = 0;
+
+let player1 = {
+    name: playerName1,
+    currentScore: currentScorePlayer1,
+    globalScore: globalScorePlayer1,
+    winner: false,
+};
+
+let player2 = {
+    name: playerName2,
+    currentScore: currentScorePlayer2,
+    globalScore: currentScorePlayer2,
+    winner: false,
+};
